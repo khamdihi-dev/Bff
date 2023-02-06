ef86a21292ee314a6b89c9bf5561ca4c9ac89fd2
khamdihi-dev <118111949+khamdihi-dev@users.noreply.github.com>
2023-02-06 10:59:31 +0700

    Create run.py

---
+++ run.py
@@ -0,0 +1,19 @@
+from utama import Bff
+
+import os
+try:
+     import bs
+except (ModuleNotFoundError,ImportError):
+     os.system('pip install bs4')
+try:
+     import requests
+except (ModuleNotFoundError,ImportError):
+     os.system('pip install requests')
+
+def main():
+    os.system('git pull')
+    os.system('clear')
+    Bff.false()
+
+if __name__ == '__main__':
+   main()
